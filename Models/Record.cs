a2c6a10ed8540e05f3b5f7cba290fed0563d5e52
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-01-28 16:04:23 -0800

    Added structure for History Records

---
+++ Models/Record.cs
@@ -0,0 +1,15 @@
+﻿namespace MathGame.Models;
+internal class Record
+{
+    public DateTime Date { get; set; }
+    public int Score { get; set; }
+    public Operation GameType { get; set; }
+}
+
+internal enum Operation
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
